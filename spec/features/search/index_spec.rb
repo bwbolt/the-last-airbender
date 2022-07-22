3899e288ee5bc5f6a8cbc327312e90e643cc64c3
Bryce Wein <bryce.wein@gmail.com>
2022-07-22 11:22:04 -0400

    Test: add test for total number of residents

---
+++ spec/features/search/index_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe '#index' do
+  it 'displays nation details after selection' do
+    visit root_path
+    select 'Fire Nation', from: 'nation'
+    click_on 'Search For Members'
+
+    expect(current_path).to eq(search_path)
+
+    expect(page).to have_content('Total Number of Fire Nation Residents:')
+  end
+end
