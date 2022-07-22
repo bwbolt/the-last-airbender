1ea47f8573389829010243d99b3b10fd78217726
Bryce Wein <bryce.wein@gmail.com>
2022-07-22 12:58:57 -0400

    Test: add specific instance to test

--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -10,6 +10,11 @@ RSpec.describe '#index' do
 
     expect(page).to have_content('Total Number of Residents: 97')
 
+    expect(page).to have_content('Chan (Fire Nation admiral)')
+    expect(page).to have_content('Allies: Ozai')
+    expect(page).to have_content('Enemies: Earth Kingdom')
+    expect(page).to have_content('Affiliations: Fire Nation Navy')
+
     expect(page).to have_content('Allies:', maximum: 25)
     expect(page).to have_content('Enemies:', maximum: 25)
     expect(page).to have_content('Affiliations:', maximum: 25)
