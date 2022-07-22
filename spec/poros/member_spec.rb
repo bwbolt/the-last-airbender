50831709ac108c6c76927afb1aadb6ddaed8250a
Bryce Wein <bryce.wein@gmail.com>
2022-07-22 12:52:48 -0400

    Test: add ignore test for model

--- spec/poros/member_spec.rb
+++ spec/poros/member_spec.rb
@@ -23,4 +23,24 @@ RSpec.describe Member do
     expect(member.affiliation).to eq('Fire Nation Navy')
     expect(member.photo).to eq('https://vignette.wikia.nocookie.net/avatar/images/a/a5/Circus_master.png/revision/latest?cb=20130706153819')
   end
+
+  it 'ignores fields not specified' do
+    data = {
+      "_id": '5cf5679a915ecad153ab68fd',
+      "allies": [
+        'Ozai'
+      ],
+      "enemies": [
+        'Earth Kingdom'
+      ],
+      "cow": 'yes',
+      "photoUrl": 'https://vignette.wikia.nocookie.net/avatar/images/a/a5/Circus_master.png/revision/latest?cb=20130706153819',
+      "name": 'Chan (Fire Nation admiral)',
+      "affiliation": 'Fire Nation Navy'
+    }
+
+    member = Member.new(data)
+
+    expect(member).to_not respond_to(:cow)
+  end
 end
