be0e1ce483da67add64fa57e3d9d61cfe8c812f6
Bryce Wein <bryce.wein@gmail.com>
2022-07-22 12:54:28 -0400

    Test: add sad path for facade

--- spec/facades/airbender_facade_spec.rb
+++ spec/facades/airbender_facade_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe 'AirbenderFacade' do
     expect(members).to be_a(Array)
     expect(members).to be_all(Member)
   end
+
+  it 'returns an array if not applicable' do
+    members = AirbenderFacade.create_members('cow')
+
+    expect(members).to be_a(Array)
+    expect(members).to eq([])
+  end
 end
